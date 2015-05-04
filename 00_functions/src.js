f8a4d2d4b5abaf104fce07c1e435cda7068449c0
Cameron Webster <cameron.webster@gmail.com>
2015-05-04 09:31:45 -0600

    tests pass for 00_functions

--- 00_functions/src.js
+++ 00_functions/src.js
@@ -1,10 +1,35 @@
 module.exports = {
 
   helloWorld: function(){
-    // fill me in
+    return "Hello World";
   },
 
   hello: function(string){
-    // fill me in
+    return "Hello " + string;
+  },
+
+  shout: function(string){
+    var stringShout = string.toUpperCase();
+    return "Hello " + stringShout;
+  },
+
+  whisper: function(string){
+    var stringWhisper = string.toLowerCase();
+    return "Hello " + stringWhisper;
+  },
+
+  separate: function(string){
+    return string.split("");
+  },
+
+  reverseJoin: function(array){
+    return array.reverse().join(",");
+  },
+  reverseJoinCompact: function(array){
+    return array.reverse().join("");
+  },
+  backwardsDay: function(string){
+    var stringReversed = string.split("").reverse().join("");
+    return "Hello " + stringReversed;
   }
 }
